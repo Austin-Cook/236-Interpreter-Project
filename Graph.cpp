555eaa183ae826ca309ea962f6fb869040b4ee44
Austin-Cook <austin.cook.id@gmail.com>
2022-11-22 10:01:09 -0700

    Implement and test dfsf's

--- Graph.cpp
+++ Graph.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Graph.h"
+#include <stack>
 
 Graph::Graph(std::vector<Rule*> const ruleVector) {
 	// copy ruleVector and build idIndexMap
@@ -17,6 +18,49 @@ Graph::Graph(std::vector<Rule*> const ruleVector) {
 	std::cout << std::endl; // FIXME DELETEME
 
 	buildDependencyGraphs();
+
+	// run dfsf on reverse dependency graph using order of low to high number
+	std::vector<int> orderForFirstDFSF;	// Should be index 0 counting up to n-1
+	for(int i = 0; i < ruleVector.size(); i++) {
+		orderForFirstDFSF.push_back(i);
+	}
+	std::vector<int> postOrderVector = dfsf_ReturnsPostOrdering(orderForFirstDFSF, reverseDependencyGraph);
+
+	// reverse the postOrder
+	std::vector<int> reversedPostOrderVector = reverseVector(postOrderVector);
+
+	// run dfsf on forward dependency graph using
+	std::set<std::set<int>> forestSet = dfsf_ReturnsForest(reversedPostOrderVector, dependencyGraph);
+
+	// FIXME DELETEME
+	std::cout << "postorderVector:" << std::endl;	// FIXME DELETME
+	for(int i = 0; i < postOrderVector.size(); i++) {
+		std::cout << postOrderVector.at(i);
+		if(i < postOrderVector.size() - 1) {
+			std::cout << ", ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::cout << "reversedPostOrderVector:" << std::endl; // FIXME DELETEME
+	for(int i = 0; i < reversedPostOrderVector.size(); i++) {
+		std::cout << reversedPostOrderVector.at(i);
+		if(i < reversedPostOrderVector.size() - 1) {
+			std::cout << ", ";
+		}
+	}
+	std::cout << std::endl;
+
+	std::cout << "Forest:" << std::endl;
+	int tempNum = 0;
+	for(auto tree : forestSet) {
+		std::cout << "SCC" << tempNum << ": ";
+		for(auto component : tree) {
+			std::cout << std::to_string(component) << ", ";
+		}
+		std::cout << std::endl;
+		tempNum++;
+	}
 }
 
 void Graph::buildDependencyGraphs() {
@@ -52,6 +96,120 @@ void Graph::addAdjacentRule(int keyNode, int valueNode) {
 	reverseDependencyGraph[valueNode].insert(keyNode);
 }
 
+// Returns -1 if the ID does not exist in the graph of rules
+// - meaning it is not a dependency and DO NOT ADD IT
+int Graph::getIndexFromId(std::string id) {
+	int indexToReturn = -1;
+
+	auto foundIterator = idIndexMap.find(id);
+	if(foundIterator != idIndexMap.end()) {
+		indexToReturn = foundIterator->second;
+	}
+
+	return indexToReturn;
+}
+
+std::vector<int> Graph::reverseVector(std::vector<int> vectorToReverse) {
+	std::stack<int> tempStack;
+	std::vector<int> reversedVector;
+	for(int i = 0; i < vectorToReverse.size(); i++) {
+		tempStack.push(vectorToReverse.at(i));
+	}
+	while(!tempStack.empty()) {
+		reversedVector.push_back(tempStack.top());
+		tempStack.pop();
+	}
+
+	return reversedVector;
+}
+
+std::vector<int> Graph::dfsf_ReturnsPostOrdering(std::vector<int> const& orderToSearchVector, std::map<int, std::set<int>>& graphToSearch){
+	std::vector<int> postOrderVector;
+	// clear visited vector
+	visitedVector.clear();
+	for(int i = 0; i < ruleVector.size(); i++) {
+		visitedVector.push_back(false);
+	}
+
+	// for each vertex in orderToSearchVector
+	for(int vertexIndex = 0; vertexIndex < orderToSearchVector.size(); vertexIndex++) {
+		// if it has NOT been visited
+		if(visitedVector.at(orderToSearchVector.at(vertexIndex)) == false) {
+			std::vector<int> postOrderVectorFromOneTree = dfs_ReturnsPostOrdering(vertexIndex, graphToSearch);
+			// add these items to postOrderVector();
+			for(int i = 0; i < postOrderVectorFromOneTree.size(); i++) {
+				postOrderVector.push_back(postOrderVectorFromOneTree.at(i));
+			}
+		}
+	}
+
+	return postOrderVector;
+}
+std::vector<int> Graph::dfs_ReturnsPostOrdering(int vertex, std::map<int, std::set<int>>& graphToSearch) {
+	std::vector<int> postOrderVector;
+	dfs_ReturnsPostOrdering_Helper(postOrderVector, vertex, graphToSearch);
+	return postOrderVector;
+}
+// Recursive helper function
+void Graph::dfs_ReturnsPostOrdering_Helper(std::vector<int>& postOrderVector, int vertex, std::map<int, std::set<int>>& graphToSearch) {
+	// mark vertex
+	visitedVector.at(vertex) = true;
+	//for each vertex adjacent from vertex
+	for(auto adjacentVertex : graphToSearch[vertex]) {
+		//if adjacentVertex is not marked
+		if(visitedVector.at(adjacentVertex) == false) {
+			dfs_ReturnsPostOrdering_Helper(postOrderVector, adjacentVertex, graphToSearch); // FIXME Set this = to an object of the post ordering that is returned
+		}
+	}
+
+	// postorder append before popping vertex off stack
+	postOrderVector.push_back(vertex);
+}
+
+std::set<std::set<int>> Graph::dfsf_ReturnsForest(std::vector<int> const& orderToSearchVector, std::map<int, std::set<int>>& graphToSearch) {
+	std::set<std::set<int>> SCCSets;
+
+	// calls the helper
+	// clear visited vector
+	visitedVector.clear();
+	for(int i = 0; i < ruleVector.size(); i++) {
+		visitedVector.push_back(false);
+	}
+
+	// for each vertex in orderToSearchVector
+	for(int vertexIndex = 0; vertexIndex < orderToSearchVector.size(); vertexIndex++) {
+		// if it has NOT been visited
+		if(visitedVector.at(orderToSearchVector.at(vertexIndex)) == false) {
+			std::cout << "Starting new tree with: " << vertexIndex << std::endl;
+			SCCSets.insert(dfs_ReturnsSearchTree(orderToSearchVector.at(vertexIndex), graphToSearch));
+		}
+	}
+
+	return SCCSets;
+}
+std::set<int> Graph::dfs_ReturnsSearchTree(int vertex, std::map<int, std::set<int>>& graphToSearch) {
+	std::set<int> searchTreeSet;
+	dfs_ReturnsSearchTree_Helper(searchTreeSet, vertex, graphToSearch);
+
+	return searchTreeSet;
+}
+// Recursive helper function
+void Graph::dfs_ReturnsSearchTree_Helper(std::set<int>& treeSet, int vertex, std::map<int, std::set<int>>& graphToSearch) {
+	// mark vertex
+	visitedVector.at(vertex) = true;
+	//for each vertex adjacent from vertex
+	for(auto adjacentVertex : graphToSearch[vertex]) {
+		//if adjacentVertex is not marked
+		if(visitedVector.at(adjacentVertex) == false) {
+			dfs_ReturnsSearchTree_Helper(treeSet, adjacentVertex, graphToSearch);
+		}
+	}
+
+	// searchTree insert before popping vertex off stack
+	treeSet.insert(vertex);
+	std::cout << "Inserting at: " << std::to_string(vertex) << std::endl;
+}
+
 std::string Graph::dependencyGraphToString() {
 	std::string stringToReturn = "";
 	// for each rule
@@ -81,17 +239,3 @@ std::string Graph::reverseDependencyGraphToString() {
 
 	return stringToReturn;
 }
-
-// Returns -1 if the ID does not exist in the graph of rules
-// - meaning it is not a dependency and DO NOT ADD IT
-int Graph::getIndexFromId(std::string id) {
-	int indexToReturn = -1;
-
-	auto foundIterator = idIndexMap.find(id);
-	if(foundIterator != idIndexMap.end()) {
-		indexToReturn = foundIterator->second;
-	}
-
-	return indexToReturn;
-}
-
--- Graph.h
+++ Graph.h
@@ -15,6 +15,7 @@ private:
 	std::vector<Rule*> ruleVector;
 	// contains the name of each node, and a pointer to the node
 	std::map<std::string, int> idIndexMap;
+	std::vector<bool> visitedVector;
 	// each int is a node, which maps to a set of adjacent nodes
 	std::map<int, std::set<int>> dependencyGraph;
 	std::map<int, std::set<int>> reverseDependencyGraph;
@@ -22,11 +23,20 @@ public:
 	Graph(std::vector<Rule*> ruleVector);
 	void buildDependencyGraphs();
 	void addEmptyRule(int keyNode);
-	// adds adjacent node to both dependency and reverse dependency graphs
 	void addAdjacentRule(int keyNode, int valueNode);
+	int getIndexFromId(std::string id);
+
+	std::vector<int> reverseVector(std::vector<int> vectorToReverse);
+
+	std::vector<int> dfsf_ReturnsPostOrdering(std::vector<int> const& orderToSearchVector, std::map<int, std::set<int>>& graphToSearch);
+	std::vector<int> dfs_ReturnsPostOrdering(int vertex, std::map<int, std::set<int>>& graphToSearch);
+	void dfs_ReturnsPostOrdering_Helper(std::vector<int>& postorderVector, int vertex, std::map<int, std::set<int>>& graphToSearch);
+	std::set<std::set<int>> dfsf_ReturnsForest(std::vector<int> const& orderToSearchVector, std::map<int, std::set<int>>& graphToSearch);
+	std::set<int> dfs_ReturnsSearchTree(int vertex, std::map<int, std::set<int>>& graphToSearch);
+	void dfs_ReturnsSearchTree_Helper(std::set<int>& treeSet, int vertex, std::map<int, std::set<int>>& graphToSearch);
+
 	std::string dependencyGraphToString();
 	std::string reverseDependencyGraphToString();
-	int getIndexFromId(std::string id);
 };
 
 
